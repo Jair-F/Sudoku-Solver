bca2947b6d5be3cae488c7e784432175464ae1ea
Jair-F <jair.fehlauer@gmail.com>
2021-10-23 20:18:22 +0300

    ad. sudokuboard at func para. to elim. global var.

--- Sudoku-Solver.py
+++ Sudoku-Solver.py
@@ -45,7 +45,7 @@ sudoku_board = [
     [None, None, None, None, None, None, None, None, None]
 ]
 
-def get_numbers_in_row(row:int) -> list:
+def get_numbers_in_row(sudoku_board:list[list], row:int) -> list:
     """
         return the numbers in the row(for example A)
     """
@@ -55,7 +55,7 @@ def get_numbers_in_row(row:int) -> list:
             ret.append(colum)
     return ret
 
-def get_numbers_in_colum(colum:int) -> list:
+def get_numbers_in_colum(sudoku_board:list[list], colum:int) -> list:
     """
     """
     ret = list()
@@ -65,7 +65,7 @@ def get_numbers_in_colum(colum:int) -> list:
             ret.append(grid)
     return ret
 
-def get_numbers_in_grid(begin_row:int, begin_colum:int) -> list:
+def get_numbers_in_grid(sudoku_board:list[list], begin_row:int, begin_colum:int) -> list:
     """
         specify one point in the sudoku_board and the function will return the numbers set in this grid
     """    
@@ -83,13 +83,13 @@ def get_numbers_in_grid(begin_row:int, begin_colum:int) -> list:
                 ret.append(num)
     return ret
 
-def check_sudoku_board() -> bool:
+def check_sudoku_board(sudoku_board:list[list], ) -> bool:
     """
         returns true if the board is fully solved!
     """
     for i in range(9):  # Check the the horizontal and vertical lines
-        row_nums   = get_numbers_in_row(i)
-        colum_nums = get_numbers_in_colum(i)
+        row_nums   = get_numbers_in_row(sudoku_board, i)
+        colum_nums = get_numbers_in_colum(sudoku_board, i)
         row_nums.sort()
         colum_nums.sort()
         
@@ -103,7 +103,7 @@ def check_sudoku_board() -> bool:
 
     for rows in range(0, 6 + 1, 3):   #checking the grids
         for colums in range(0, 6 + 1, 3):
-            grid_nums  = get_numbers_in_grid(rows, colums)
+            grid_nums  = get_numbers_in_grid(sudoku_board, rows, colums)
             grid_nums.sort()
 
             for a in range(9):
@@ -121,8 +121,8 @@ def check_duplicate_numbers(sudoku_board:list[list]) -> bool:
     colum_nums = list
     grid_nums  = list
     for i in range(9):  # get the horizontal and vertical lines
-        row_nums   = get_numbers_in_row(i)
-        colum_nums = get_numbers_in_colum(i)
+        row_nums   = get_numbers_in_row(sudoku_board, i)
+        colum_nums = get_numbers_in_colum(sudoku_board, i)
 
         row_nums.sort()
         colum_nums.sort()
@@ -136,7 +136,7 @@ def check_duplicate_numbers(sudoku_board:list[list]) -> bool:
     
     for rows in range(0, 6 + 1, 3):   # get the grids
         for colums in range(0, 6 + 1, 3):
-            grid_nums  = get_numbers_in_grid(rows, colums)
+            grid_nums  = get_numbers_in_grid(sudoku_board, rows, colums)
 
             grid_nums.sort()
             for i in range(1, len(grid_nums)):
@@ -163,7 +163,7 @@ def solve_sudoku(sudoku_board:list[list]) -> bool:
     if check_duplicate_numbers(sudoku_board) == True:
         return False
     if check_fully_filled(sudoku_board) == True:
-        return check_sudoku_board() # if the board is right, it will return true else false
+        return check_sudoku_board(sudoku_board) # if the board is right, it will return true else false
 
 
     """
@@ -200,7 +200,7 @@ def solve_sudoku(sudoku_board:list[list]) -> bool:
             if sudoku_board[row][colum] != None:    # if at this position there is already a number - continue - we dont can put in there a number
                 continue
 
-            used_numbers = get_numbers_in_colum(colum) + get_numbers_in_row(row) + get_numbers_in_grid(row, colum)
+            used_numbers = get_numbers_in_colum(sudoku_board, colum) + get_numbers_in_row(sudoku_board, row) + get_numbers_in_grid(sudoku_board, row, colum)
             used_numbers = list(set(used_numbers)) # remove duplicates - we added the numbers in the row,colum and grid - there are maybe duplicates
             
             num_of_options = len(all_options) - len(used_numbers)
